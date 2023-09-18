f2d4f775b89c505cb93b45b1fa9d7867faffc298
DESKTOP-PDPG76T\Nikola <nikolagpetrov97@gmail.com>
2023-09-18 12:14:16 +0300

    form reset fix

--- client/src/components/Form/Form.tsx
+++ client/src/components/Form/Form.tsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import "./Form.css";
 
 interface Props {
@@ -7,21 +8,28 @@ interface Props {
 }
 
 const Form = ({ action, children, onSubmit }: Props) => {
+  const formRef = useRef<HTMLFormElement>(null);
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
     const formElement = e.target as HTMLFormElement;
     const isValid = formElement.checkValidity();
-
-    // submit the dataObject if isValid===true
     if (isValid) {
       const dataObject = new FormData(formElement);
       onSubmit(dataObject);
+      if (formRef.current) {
+        formRef?.current.reset();
+      }
     }
   };
 
   return (
-    <form action={action} onSubmit={handleSubmit} noValidate className="form">
+    <form
+      ref={formRef}
+      action={action}
+      onSubmit={handleSubmit}
+      noValidate
+      className="form"
+    >
       {children}
     </form>
   );
